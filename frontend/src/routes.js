12800bb65887d98e3f739e55846b6839623f0bc7
Michael Abrams <msabrams@ncsu.edu>
2023-12-02 18:28:19 -0500

    [Implementation] Created movie modal, and watch list for user

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,6 +7,7 @@ const cookieParser = require('cookie-parser');
 // Set the paths for the static resources and templates
 routes.use(express.static(__dirname));
 const path = require('path');
+const { rmSync } = require('fs');
 const html_dir = path.join(__dirname, './template/');
 
 // Parsers for cookies and body text
@@ -14,20 +15,24 @@ routes.use(cookieParser());
 routes.use(bodyParser.urlencoded({ extended: false}));
 
 // Define the routes for the frontend pages here
-routes.get('/', (req, res) => {
+routes.get('/', (_, res) => {
     res.sendFile(`${html_dir}splashpage.html`);
 });
 
-routes.get('/signin', (req, res) => {
+routes.get('/signin', (_, res) => {
     res.sendFile(`${html_dir}signin.html`);
 });
 
-routes.get('/signup', (req, res) => {
+routes.get('/signup', (_, res) => {
     res.sendFile(`${html_dir}signup.html`);
 });
 
-routes.get('/search', (req, res) => {
-    res.sendFile(`${html_dir}search.html`)
+routes.get('/explore', (_, res) => {
+    res.sendFile(`${html_dir}search.html`);
+})
+
+routes.get('/movie-collection', (_, res) => {
+    res.sendFile(`${html_dir}movie-collection.html`);
 })
 
 // Export this code to be used for other scripts

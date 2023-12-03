01c51362a6c24b7da816d20c354b13267dd0a1c4
Noah Thomas <nbthomas@ncsu.edu>
2023-12-03 18:57:58 -0500

    sharing updated searchRoutes with merger.

--- api/src/routes/searchRoutes.js
+++ api/src/routes/searchRoutes.js
@@ -7,37 +7,6 @@ module.exports = router;
 
 
 // Search
-<<<<<<< HEAD
-router.get('/search', (req, res) => {
-    const title = req.body.title;
-    const desc =  req.body.desc
-    const mediaType = req.body.mediaType;
-    const publisher = req.body.publisher;
-    const staff = req.body.staff;
-    const runtimeMin = req.body.runtimeMin;
-    const runtimeMax = req.body.runtimeMax;
-    const year = req.body.year;
-    const ageRating = req.body.ageRating;
-    const language = req.body.language;
-    const streamingService = req.body.streamingService;
-    const genre = req.body.genre;
-
-    const query = "SELECT * FROM Media WHERE Title LIKE ? AND Description LIKE ?";
-
-    db.query(query,['%'+title+'%', '%'+desc+'%']).then(results => {
-        return res.status(200).json({data: results});
-    });
-    
-});
-
-//Get all Media
-router.get('/media', (res) => {
-    db.query("SELECT * FROM Media").then(results => {
-        return res.status(200).json({data: results});
-    });
-})
-=======
->>>>>>> development
 
 // Get all Media Types
 router.get('/mediatype', (req, res) => {

b47cc6887ecf5ef745661ee1aad2c287d082a15e
Noah Thomas <nbthomas@ncsu.edu>
2023-12-04 11:04:40 -0500

    updated the put media api call to also include new entries in the mediastreaming and mediagenre tables to model their many to one relations.

--- api/src/routes/mediaRoutes.js
+++ api/src/routes/mediaRoutes.js
@@ -9,11 +9,25 @@ module.exports = router;
 router.put('/', (req, res) => {
     const media = req.body;
 
+    // Get steaming service id
+    const streamingQuery = "SELECT StreamingServiceId FROM StreamingService WHERE StreamingServiceName = ?";
+
+    serviceId = db.query(streamingQuery, [media.streaming])
+
+    const genreQuery = "SELECT GenreId FROM Genre WHERE GenreName = ?";
+    genreId = db.query(genreQuery, [media.genre])
+
     const query = "INSERT INTO Media (Title, Description, ReleaseDate, RunTime, Poster, MediaTypeId, PublisherId, LanguageId, AgeRatingId) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
     
     db.query(query,[media.title, media.description, media.releaseDate, media.runTime, media.poster, media.mediaType, media.publisher, media.language, media.ageRating]).then(results => {
+        const mediaStreamingPairQuery = "INSERT INTO MediaStreamingService (MediaId, StreamingServiceId) VALUES (?, ?)";
+        db.query(mediaStreamingPairQuery, [results.insertId, serviceId]);
+        const mediaGenrePairQuery = "INSERT INTO MediaGenre (MediaId, GenreId) VALUES (?, ?)";
+        db.query(mediaGenrePairQuery, [results.insertId, genreId]);
         return res.status(200).json({data: results});
     });    
+
+    //
 
 });
 

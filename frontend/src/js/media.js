7c494b128e127de548aabb688c519f7a2f42e210
Michael Abrams <msabrams@ncsu.edu>
2023-12-05 11:31:51 -0500

    [Implementation] Fixed watchlist glitch

--- frontend/src/js/media.js
+++ frontend/src/js/media.js
@@ -303,10 +303,10 @@ export async function loadMedia(movies, fromWatchList) {
         const modal = new bootstrap.Modal(document.getElementById(`modal${media.MediaId}`));
 
         // Add an event listener for the hidden.bs.modal event
-        modal._element.addEventListener('hidden.bs.modal', function () {
+        modal._element.addEventListener('hidden.bs.modal', async function () {
             if(fromWatchList) {
-                const response = APIClient.fetchUserList(user.id);
-                loadMedia(response, true)
+                const response = await APIClient.fetchUserList(user.id);
+                loadMedia(response.data.results, true)
             }
         });
 
